4f6aa23d2cc2d32959f8009c6d535f76d6ca1d44
MardonioEng <mardoniorodrigues11@gmail.com>
2020-11-11 09:48:05 -0300

    View de novo artigo e TinyMCE

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 router.get('/articles', (req, res) => {
     res.send("Rota para artigos");
-})
+});
+
+router.get('/admin/articles/new', (req, res) => {
+    res.render('admin/articles/new');
+});
 
 module.exports = router;
