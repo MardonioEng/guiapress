d99084db32e61f949c9deb9341c8973b65dbe9c4
MardonioEng <mardoniorodrigues11@gmail.com>
2020-11-19 10:09:42 -0300

    Listagem Usuarios e impedir e-mail duplicado

--- users/usersController.js
+++ users/usersController.js
@@ -5,7 +5,11 @@ const bcrypt = require('bcryptjs');
 const User = require('./User');
 
 router.get('/admin/users', (req, res) => {
-    res.send("Listagem de usuários");
+    User.findAll().then(users => {
+        res.render('admin/users/index', {
+            users: users
+        });
+    });
 });
 
 router.get('/admin/users/create', (req, res) => {
@@ -16,16 +20,24 @@ router.post('/users/create', (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    let salt = bcrypt.genSaltSync(10);
-    let hash = bcrypt.hashSync(password, salt);
+    User.findOne({where: {email: email}}).then(user => {
+        if(user == undefined) {
 
-    User.create({
-        email: email,
-        password: hash
-    }).then(() => {
-        res.redirect("/");
-    }).catch((err) => {
-        res.redirect("/");
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(password, salt);
+        
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect("/");
+            }).catch((err) => {
+                res.redirect("/");
+            });
+
+        }else{
+            res.render('admin/users/create');
+        }
     });
 
 });
